663a5f79940d5efd6dab32e78652db14830e2f03
dacoroman-ottawa <62803075+dacoroman-ottawa@users.noreply.github.com>
2022-02-20 13:57:48 -0500

    Solved NoClassDefFoundError JAXBException

--- src/test/java/guru/springfamework/spring5mvcrest/spring5mvcrest/Spring5MvcRestApplicationTests.java
+++ src/test/java/guru/springfamework/spring5mvcrest/spring5mvcrest/Spring5MvcRestApplicationTests.java
@@ -1,11 +1,8 @@
-package guru.springfamework.spring5mvcrest;
+package guru.springfamework.spring5mvcrest.spring5mvcrest;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5MvcRestApplicationTests {
 
